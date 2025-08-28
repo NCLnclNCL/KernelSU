4ece26783f1ba70caab0d6893b2adcd5ea084239
NCLnclNCL <nanvanu@gmail.com>
2025-08-28 11:58:18 +0700

    enable devpts hook

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -143,8 +143,13 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 	return 0;
 }
 
-static int ksu_inline_handle_devpts(struct inode *inode)
+int ksu_handle_devpts(struct inode *inode)
 {
+#ifndef CONFIG_KSU_KPROBES_HOOK
+	if (!ksu_sucompat_hook_state) {
+		return 0;
+	}
+#endif
 	if (!current->mm) {
 		return 0;
 	}
@@ -170,21 +175,6 @@ static int ksu_inline_handle_devpts(struct inode *inode)
 		}
 	}
 
-	return 0;
-}
-
-int __ksu_handle_devpts(struct inode *inode)
-{
-#ifndef CONFIG_KSU_KPROBES_HOOK
-	if (!ksu_sucompat_hook_state)
-		return 0;
-#endif
-	return ksu_inline_handle_devpts(inode);
-}
-
-// dead code, we are phasing out ksu_handle_devpts for LSM hooks.
-int __maybe_unused ksu_handle_devpts(struct inode *inode)
-{
 	return 0;
 }
 
@@ -234,7 +224,7 @@ static int pts_unix98_lookup_pre(struct kprobe *p, struct pt_regs *regs)
 	inode = (struct inode *)PT_REGS_PARM2(regs);
 #endif
 
-	return ksu_inline_handle_devpts(inode);
+	return ksu_handle_devpts(inode);
 }
 #else
 static struct kprobe *su_kps[5];
