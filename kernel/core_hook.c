988159773227c0d966564242f37cdc7ba8632909
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-06 01:39:21 +0700

    Update core_hook.c

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -1030,12 +1030,10 @@ out_ksu_try_umount:
 		pr_info("uid: %d should not umount!\n", current_uid().val);
 #endif
 	}
-#ifndef CONFIG_KSU_SUSFS_SUS_MOUNT
 	// check old process's selinux context, if it is not zygote, ignore it!
 	// because some su apps may setuid to untrusted_app but they are in global mount namespace
 	// when we umount for such process, that is a disaster!
 	 bool is_zygote_child = ksu_is_zygote(old->security);
-#endif
 	if (!is_zygote_child) {
 		pr_info("handle umount ignore non zygote child: %d\n",
 			current->pid);
