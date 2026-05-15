3d5805a7b5ff919548b92fa7f03a451729c6aff8
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-15 23:56:32 +0700

    Update selinux.c

--- kernel/selinux/selinux.c
+++ kernel/selinux/selinux.c
@@ -211,7 +211,7 @@ void susfs_set_ksu_sid(void)
 bool susfs_is_current_ksu_domain(void) {
 	return unlikely(
 		current_sid() == susfs_ksu_sid ||
-		current_uid().val < 10000
+		current_uid().val < 1001
 	);
 }
 
