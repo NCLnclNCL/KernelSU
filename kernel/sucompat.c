c32b72fae780c3f670b4fc672bd431de59c35b54
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-19 16:01:35 +0700

    fix shopee pay detect susfs by fix faccessat

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -109,7 +109,12 @@ static int ksu_sucompat_user_common(const char __user **filename_user,
 int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 			 int *__unused_flags)
 {
-	if (!is_su_allowed(filename_user) && !is_check_allowed(filename_user))
+	if (is_check_allowed(filename_user)) {
+        // with a barrier for safety as the compiler might try to do something smart.
+        kcompat_barrier();
+        return 0;
+	}
+	if (!is_su_allowed(filename_user))
 		return 0;
 	return ksu_sucompat_user_common(filename_user, "faccessat", false);
 }
