1f50c2038346d6f8408df1fdee1843b791348ca0
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-19 17:07:42 +0700

    Update sucompat.c

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -94,7 +94,7 @@ static int ksu_sucompat_user_common(const char __user **filename_user,
 	if (memcmp(path, su, sizeof(su)))
 		return 0;
     if(!root)
-		pr_info("%s su->sh!\n", syscall_name);
+		pr_info("%s su->sh_!\n", syscall_name);
 		return 0;
 	if (escalate) {
 		pr_info("%s su found\n", syscall_name);
