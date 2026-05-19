3c585eb05896b007160aa0bd2fe93a65a2f4a779
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-19 15:05:47 +0700

    Update sucompat.c

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -95,19 +95,15 @@ static int ksu_sucompat_user_common(const char __user **filename_user,
 int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 			 int *__unused_flags)
 {
-	uid_t current_uid_val = current_uid().val;
 	if (!is_su_allowed(filename_user))
 		return 0;
-	pr_info("ksu_handle_faccessat by: %d with process: %s\n", __func__, current_uid_val, current->comm);
 	return ksu_sucompat_user_common(filename_user, "faccessat", false);
 }
 
 int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
-	uid_t current_uid_val = current_uid().val;
 	if (!is_su_allowed(filename_user))
 		return 0;
-	pr_info("ksu_handle_stat by: %d with process: %s\n", __func__, current_uid_val, current->comm);
 	return ksu_sucompat_user_common(filename_user, "newfstatat", false);
 }
 
