a6fcc7241057b5765d396dc6d9829a6647625d4b
Admin <nanvanu@gmail.com>
2026-05-20 11:15:23 +0700

    Revert "kernel: drop LSM_HOOK"
    This reverts commit 1a301e3b8c876ddec991be156e799a3ef67e3a29.

--- kernel/hook/lsm_hook.c
+++ kernel/hook/lsm_hook.c
@@ -1,6 +1,12 @@
+#ifdef CONFIG_KSU_LSM_HOOKS
+#define LSM_HOOK_TYPE static int
+#else
+#define LSM_HOOK_TYPE int
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI) ||                                     \
     defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
-static int ksu_key_permission(key_ref_t key_ref, const struct cred *cred, unsigned perm)
+LSM_HOOK_TYPE ksu_key_permission(key_ref_t key_ref, const struct cred *cred, unsigned perm)
 {
     if (init_session_keyring != NULL) {
         return 0;
@@ -16,7 +22,7 @@ static int ksu_key_permission(key_ref_t key_ref, const struct cred *cred, unsign
 }
 #endif
 
-static int ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
+LSM_HOOK_TYPE ksu_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
 {
     uid_t new_uid, old_uid = 0;
 
@@ -63,7 +69,7 @@ void __init ksu_lsm_hook_init(void)
     pr_info("LSM hooks initialized.\n");
 }
 #else
-void __init ksu_lsm_hook_init(void)
+void __init ksu_lsm_hook_init()
 {
 } /* no opt */
 #endif
